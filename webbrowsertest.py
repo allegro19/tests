40d7910545dc12fb173c62d04ee5a8f7295dd036
Amelie Aylesbury <amelie@localhost.localdomain>
2017-01-05 17:30:20 +0000

    doing a cool test program

---
+++ webbrowsertest.py
@@ -0,0 +1,9 @@
+import webbrowser
+import time
+rest=3
+while rest>0:
+  #  webbrowser.open('https://www.youtube.com/watch?v=VyR7yoDBQSg')
+    time.sleep(10)
+    webbrowser.open('https://www.google.co.uk/search?q=space&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiT5_aCwavRAhXrJMAKHaAgCM0Q_AUICCgB&biw=1440&bih=765#imgrc=odMuJYuFGbccxM%3A')
+    rest=rest-1
+
