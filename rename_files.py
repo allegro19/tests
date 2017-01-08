c3744edc55a540e3e3af2a6e4e480e2c9da92fc1
Amelie Aylesbury <amelie@localhost.localdomain>
2017-01-08 14:52:46 +0000

    secret messege

---
+++ rename_files.py
@@ -0,0 +1,10 @@
+import os
+def rename_files():
+    #1 get file names from a folder
+    file_list = os.listdir(r'C:/OOP/prank')
+    print(file_list)
+    #2 for each file, rename filename
+    for file_name in file_list:
+        os.rename(file_name, file_name.translate(None, '0123456789'))
+
+rename_files()
