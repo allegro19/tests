92b84892f0f54b7b2d0db415801e7594c84f1d6a
Amelie Aylesbury <amelie@localhost.localdomain>
2017-01-19 17:32:08 +0000

    done it more suffisticated way

--- drawing_turtle.py
+++ drawing_turtle.py
@@ -1,33 +1,34 @@
 import turtle
-def draw_square():
+
+def draw_square(some_turtle):
+    for i in range(1,6):
+        some_turtle.forward(200)
+        some_turtle.right(90)
+
+def draw_triangle(some_turtle):
+    for i in range(1,6):
+        some_turtle.forward(200)
+        some_turtle.left(120)
+
+def draw_stuff():
     window = turtle.Screen()
     window.bgcolor('purple')
-# customising my turtle
-
+    #Creating steve -draws a square
     steve = turtle.Turtle()
     steve.shape('turtle')
     steve.color('green')
     steve.speed(2)
-#making my turtle move
-    line = 4
-    while line>=0:
-        steve.forward(200)
-        steve.right(90)
-        line=line-1
-        
+    draw_square(steve)
+    #Creating amelie -draws a square
     amelie = turtle.Turtle()
     amelie.shape('turtle')
     amelie.color('gold')
     amelie.circle(100)
-    
+    #creating pinky -draws a triangle    
     pinky = turtle.Turtle()
     pinky.shape('turtle')
     pinky.color('pink')
-    line =4
-    while line>=0:
-        pinky.forward(200)
-        pinky.left(120)
-        line = line-1
+    draw_triangle(pinky)
     window.exitonclick()
 
-draw_square()
+draw_stuff()
